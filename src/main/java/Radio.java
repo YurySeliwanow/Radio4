da7488c97361622d3f60ddf77befe7671d44d990
YurySeliwanow <131137284+YurySeliwanow@users.noreply.github.com>
2023-07-11 22:23:02 +0300

    first commit

---
+++ src/main/java/Radio.java
@@ -0,0 +1,64 @@
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+
+    public void nextStation() {
+        if (currentStation != 9) {
+            currentStation++;
+        } else {
+            currentStation = 0;
+        }
+    }
+
+    public void prevStation() {
+        if (currentStation != 0) {
+            currentStation--;
+        } else {
+            currentStation = 9;
+        }
+    }
+
+    public void nextVolume() {
+        if (currentVolume != 100) {
+            currentVolume++;
+        } else {
+            return;
+        }
+    }
+
+    public void prevVolume() {
+        if (currentVolume != 0) {
+            currentVolume--;
+        } else {
+            return;
+        }
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) {
+            return;
+        }
+        if (currentStation > 9) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+        }
+        if (currentVolume > 100) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+}
